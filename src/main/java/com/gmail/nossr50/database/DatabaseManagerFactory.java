c9de70292a7313802533e94fe77a505df024ce75
riking <rikingcoding@gmail.com>
2013-06-08 16:29:48 -0700

    Add "volatile API" for a custom DatabaseManager
    Reflection is fun! Catch-alls abound!

--- src/main/java/com/gmail/nossr50/database/DatabaseManagerFactory.java
+++ src/main/java/com/gmail/nossr50/database/DatabaseManagerFactory.java
@@ -1,9 +1,48 @@
 package com.gmail.nossr50.database;
 
+import com.gmail.nossr50.mcMMO;
 import com.gmail.nossr50.config.Config;
 
 public class DatabaseManagerFactory {
+    private static Class<? extends DatabaseManager> customManager = null;
+
+    /**
+     * Sets the custom DatabaseManager class for McMMO to use. This should be
+     * called prior to mcMMO enabling.
+     * <p>
+     * The provided class must have an empty constructor, which is the one
+     * that will be used.
+     * <p>
+     * This method is intended for API use, but it should not be considered
+     * stable. This method is subject to change and/or removal in future
+     * versions.
+     *
+     * @param man the DatabaseManager class to use
+     * @throws IllegalArgumentException if the provided class does not have
+     *             an empty constructor
+     */
+    public static void setCustomDatabaseManagerClass(Class<? extends DatabaseManager> man) {
+        try {
+            customManager.getConstructor((Class<?>) null);
+        } catch (Throwable e) {
+            throw new IllegalArgumentException("Provided database manager class must have an empty constructor", e);
+        }
+        customManager = man;
+    }
+
     public static DatabaseManager getDatabaseManager() {
+        if (customManager != null) {
+            try {
+                return customManager.getConstructor((Class<?>) null).newInstance((Object[]) null);
+            } catch (Exception e) {
+                mcMMO.p.debug("Could not create custom database manager");
+                e.printStackTrace();
+            } catch (Throwable e) {
+                mcMMO.p.debug("Failed to create custom database manager");
+                e.printStackTrace();
+            }
+            mcMMO.p.debug("Falling back on " + (Config.getInstance().getUseMySQL() ? "SQL" : "Flatfile") + " database");
+        }
         return Config.getInstance().getUseMySQL() ? new SQLDatabaseManager() : new FlatfileDatabaseManager();
     }
 }
